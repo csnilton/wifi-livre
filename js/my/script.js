848750d615d1df5030787d7e64e0afe1e0b48c62
Gabriel Furlan <gabrielfurlan05@gmail.com>
2016-08-01 08:56:06 -0300

    Termo de segurança e privacidade

---
+++ js/my/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	var tamanhoContainer = $('.container-height #validacao').height();
+	var tamanhoGeral = $('.container-height').height();
+
+
+	$('.container-height #validacao').css({
+		"margin-top": ((tamanhoGeral / 2) - (tamanhoContainer / 2)) + "px",
+	});
+
+
+	$('#modal .modal-dialog .modal-body').css({
+		"height": (0.644444444 * tamanhoGeral) + "px",
+	});
+
+
+	var tamanhoBodyModal = $('#modal .modal-dialog .modal-body').height(); 
+	var bottomBodyModal = $('#modal .modal-dialog .modal-body').offset().top + tamanhoBodyModal;
+
+	$('#modal .modal-dialog .modal-body').scroll(function(){
+
+		if($(this).scrollTop() > bottomBodyModal) $('#modal .modal-dialog .modal-footer #concordo').removeClass('disabled');
+	});
+	
+});
